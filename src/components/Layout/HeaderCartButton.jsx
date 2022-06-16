18ed3dd227a5607b51ec1982a150255a2e6af492
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-15 20:59:49 -0400

    Finished practice project. Added small animation bump

--- src/components/Layout/HeaderCartButton.jsx
+++ src/components/Layout/HeaderCartButton.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
@@ -6,9 +6,26 @@ import CartIcon from "../Cart/CartIcon";
 
 const HeaderCartButton = (props) => {
   const { openHandler, totalItems } = useContext(CartContext);
+  const [bump, setBump] = useState(false);
+
+  const btnClasses = `${classes.button} ${bump ? classes.bump : ""}`;
+
+  useEffect(() => {
+    if (totalItems === 0) {
+      return;
+    }
+    setBump(true);
+    const timeout = setTimeout(() => {
+      setBump(false);
+    }, 100);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [totalItems]);
 
   return (
-    <button onClick={openHandler} className={classes.button}>
+    <button onClick={openHandler} className={btnClasses}>
+      {console.log(btnClasses)}
       <span className={classes.icon}>
         <CartIcon />
       </span>
